50e8da93a11d4c2374e7db988ac512480c7d5f60
rofersk <rhafelpacaostudent@gmail.com>
2021-11-22 11:34:40 +0800

    string

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,38 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <!-- This is an example component -->
+  <div class="mx-10 pt-15">
+    <div class="grid gap-6 mb-8 md:grid-cols-2">
+      <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+        <h4 class="mb-4 font-semibold text-gray-600 dark:text-gray-300">
+          Student
+        </h4>
+        <p class="text-gray-600 dark:text-gray-400">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, cum
+          commodi a omnis numquam quod? Totam exercitationem quos hic ipsam at
+          qui cum numquam, sed amet ratione! Ratione, nihil dolorum.
+        </p>
+      </div>
+      <div class="min-w-0 p-4 text-white bg-purple-600 rounded-lg shadow-xs">
+        <h4 class="mb-4 font-semibold">Administrator</h4>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, cum
+          commodi a omnis numquam quod? Totam exercitationem quos hic ipsam at
+          qui cum numquam, sed amet ratione! Ratione, nihil dolorum.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
 }
 </script>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.font-family-karla {
+  font-family: karla;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Register from './components/Register.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import VueRouter from 'vue-router'
+import vuetify from './plugins/vuetify'
+import './index.css'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/register', component: Register },
+  { path: '/', component: HelloWorld },
+]
+
+const router = new VueRouter({
+  routes,
+})
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  render: (h) => h(App),
 }).$mount('#app')
